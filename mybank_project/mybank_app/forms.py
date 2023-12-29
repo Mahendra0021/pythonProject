28b127d51e905a5d91e8beecbd6b0a5e25ed3114
Mahendra <sainmahendra171@gmail.com>
2023-12-29 18:37:19 +0530

    Extended

--- mybank_project/mybank_app/forms.py
+++ mybank_project/mybank_app/forms.py
@@ -1,8 +1,16 @@
 from django import forms
-from .models import BankAccount
+
 
 
 class BankAccountForm(forms.ModelForm):
-    class Meta:
-        model = BankAccount
-        exclude = []  # or specify the fields you want to include
+    full_name = forms.CharField(max_length=100)
+    phone_number = forms.IntegerField()
+    # id_proof = forms.CharField(max_length=20)
+    # account_type = forms.CharField(max_length=10)
+    # email_address = forms.EmailField()
+    # date_of_birth = forms.DateField()
+    residential_address = forms.CharField(max_length=100)
+    # digital_picture = forms.ImageField(upload_to='digital_pictures/')
+    citizenship = forms.CharField(max_length=50)
+    # initial_deposit = forms.DecimalField(max_digits=10, decimal_places=2)
+    # agree_t_and_c = forms.BooleanField()
--- mybank_project/mybank_app/views.py
+++ mybank_project/mybank_app/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render, redirect
-from .models import Service
+from .models import BankAccount,Service
 from .forms import BankAccountForm
 
 
 def home(request):
-    services = Service.objects.all()
+    services = BankAccount.objects.all()
     return render(request, 'home.html', {'services': services})
 
 
 def create_account(request):
-    if request.method == 'POST':
-        form = BankAccountForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')  # Redirect to home page after successful submission
-    else:
-        form = BankAccountForm()
-
-    return render(request, 'create_account.html', {'form': form})
+    if request.method == "POST":
+        full_name = request.POST['fullName']
+        phone_number = request.POST['phoneNumber']
+        residential_address = request.POST['residentialAddress']
+        citizenship = request.POST['citizenship']
+        value = BankAccount(
+         full_name=full_name,
+         phone_number=phone_number,
+         residential_address= residential_address,
+         citizenship=citizenship   
+        )
+        value.save()
+    return render(request, 'create_account.html')
